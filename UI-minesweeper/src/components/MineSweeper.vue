fb97f34c4e2a8bfaf89752224a8c6b68a55a62e9
Amir Boussejra <amir@epigno.com>
2022-06-24 22:58:11 +0200

    [frontend] vue-js frontend using vite for minesweeper

---
+++ UI-minesweeper/src/components/MineSweeper.vue
@@ -0,0 +1,105 @@
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+import init, {
+  getState,
+  openField,
+  toggleFlag,
+  resetGame,
+} from "../../../pkg/minesweeper_wasm_rust.js";
+const component = defineComponent({
+  name: "mine-sweeper",
+  setup() {
+    // Initialised variable to make sure wasm is initialised
+    let ready = ref(false);
+    // Initialised variable to update data at each click
+    let data = ref([] as string[][]);
+    return { ready, data };
+  },
+  async mounted() {
+    await init();
+    this.ready = true;
+    this.update_data();
+  },
+  methods: {
+    left_click_manager(x: number, y: number): void {
+      openField(x, y);
+      this.update_data();
+    },
+    right_click_manager(x: number, y: number): void {
+      toggleFlag(x, y);
+      this.update_data();
+    },
+    update_data() {
+      this.data = getState()
+        .split("\n")
+        .map((row) => row.trim().split(/\s+/));
+    },
+    reset(): void {
+      resetGame();
+      this.update_data();
+    },
+  },
+});
+
+export default component;
+</script>
+
+<template>
+  <div>
+    <link
+      href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
+      rel="stylesheet"
+    />
+    <h2 class="font-medium leading-tight text-4xl mt-0 mb-2 text-blue-600">
+      MineSweeper WASM Rust Master Race
+    </h2>
+    <div v-if="ready" id="root">
+      <div v-for="(d, y) in data" :key="d">
+        <div
+          class="field"
+          v-for="(e, x) in d"
+          :key="e"
+          @click="left_click_manager(x, y)"
+          @contextmenu="right_click_manager(x, y)"
+        >
+          {{ e }}
+        </div>
+      </div>
+    </div>
+    <div>
+      <button
+        id="reset"
+        class="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded-full"
+        style="font-size: 18px"
+        @click="reset()"
+      >
+        Reset game
+      </button>
+    </div>
+  </div>
+</template>
+
+<style>
+html {
+  font-size: 200%;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.field {
+  text-decoration: none;
+  text-align: center;
+  width: 1.2rem;
+  height: 1.2rem;
+  line-height: 1.2rem;
+  cursor: pointer;
+}
+#root {
+  display: inline-grid;
+  grid-template: repeat(10, auto) / repeat(10, auto);
+}
+#reset {
+  border: 1px solid black;
+  cursor: pointer;
+}
+</style>
